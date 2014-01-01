a2dee858f3362a7a7e327f59441c6e5d4d9787c4
crazycusti <crazycusti@googlemail.com>
2014-01-01 19:36:16 +0100

    added video level

---
+++ video.c
@@ -0,0 +1,12 @@
+void init(void)
+{
+	const char hw[] = "hier ist nichts.";
+	int i;
+	char* video = (char*) 0xb8000;
+	
+	for (i = 0; hw[i];i = 0) {
+			video [i * 2] = hw[i];
+			// <Commodore-Freak> schwarz gelb is beste farbenkombination - <Wynton> stimmt, bvb
+			video [i * 2 + 1] = 0xE;
+	}
+}
