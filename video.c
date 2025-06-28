0949b24224872fa4b83a5bab5a508218ec7e2d44
Wynton Grund <crazycusti@googlemail.com>
2025-06-29 00:25:14 +0200

    remove unused video code

--- video.c
+++ video.c
@@ -4,13 +4,6 @@
 
 #define VIDEO_MEMORY 0xb8000
 
-void video()
-{
-    const char video_adress = (char)0xb8000; 
-    unsigned int displaysize = 80*25*2;
-    video_adress = 0x07; 
-}
-
 void video_init() {
     volatile uint16_t* video = (uint16_t*) VIDEO_MEMORY;
     for (int y = 0; y < CONFIG_VGA_HEIGHT; y++) {
